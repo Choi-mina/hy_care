2a49ec623f9d6d870d4c3fba757aa47d31260c11
Choi-mina <cnm37@naver.com>
2023-12-07 00:39:02 +0900

    chatGPT API modify

--- src/main/java/com/example/hycare/chatGPT/testController.java
+++ src/main/java/com/example/hycare/chatGPT/testController.java
@@ -41,51 +41,52 @@ public class testController {
 
     //chat-gpt API 호출
      @PostMapping("/summary")
-     public String summary(@RequestBody Map<Integer, String> stt, @RequestParam String email) throws IOException {
+     public String summary(@RequestBody Map<Integer, String> stt, @RequestParam String email, String loginDiv) throws IOException {
 
         summary = chatService.getChatResponse(stt);
 
-         // json 파일을 저장할 상대 directory 지정
-         String path = System.getProperty("user.dir") + "/summary";
-         File Folder = new File(path);
-         // 해당 디렉토리가 없다면 디렉토리를 생성.
-         if (!Folder.exists()) {
-             try{
-                 Folder.mkdir(); //폴더 생성합니다. ("새폴더"만 생성)
-                 log.info("summary folder create success");
-             }
-             catch(Exception e){
-                 e.getStackTrace();
-             }
-         }else {
-             log.info("summary folder exists");
-         }
+        if(loginDiv.equals("1")) {   // 환자인 경우
+            // json 파일을 저장할 상대 directory 지정
+            String path = System.getProperty("user.dir") + "/summary";
+            File Folder = new File(path);
+            // 해당 디렉토리가 없다면 디렉토리를 생성.
+            if (!Folder.exists()) {
+                try{
+                    Folder.mkdir(); //폴더 생성합니다. ("새폴더"만 생성)
+                    log.info("summary folder create success");
+                }
+                catch(Exception e){
+                    e.getStackTrace();
+                }
+            }else {
+                log.info("summary folder exists");
+            }
 
-         // ChatGPT 결과 -> json file로 변환해 로컬 저장
-         ChatGPTDto chatGPTDto = new ChatGPTDto();
-         chatGPTDto.setStt(stt.values().toString());
-         chatGPTDto.setSummary(summary);
-         ObjectMapper mapper = new ObjectMapper();
-         mapper.writeValue(new File(path + "/chatGPTDto.json"), chatGPTDto);
+            // ChatGPT 결과 -> json file로 변환해 로컬 저장
+            ChatGPTDto chatGPTDto = new ChatGPTDto();
+            chatGPTDto.setStt(stt.values().toString());
+            chatGPTDto.setSummary(summary);
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.writeValue(new File(path + "/chatGPTDto.json"), chatGPTDto);
 
 
-         // S3에 저장할 수 있도록 API 호출
-         DiagnosisDto diagnosisDto = diagnosisService.findDiagnosis();
-         String url = baseUrl + "/s3-save/" + diagnosisDto.getDiagId() + "?email=" + email;
-         HttpHeaders headers = new HttpHeaders();
-         headers.setContentType(MediaType.APPLICATION_JSON);
-         HttpEntity httpEntity = new HttpEntity<>(path, headers);
-         RestTemplate restTemplate = new RestTemplate();
-         ResponseEntity<ResultEntity> response = restTemplate.exchange(
-                 url,
-                 HttpMethod.POST,
-                 httpEntity,
-                 ResultEntity.class);
-
-         if(response.getStatusCode() == HttpStatus.OK) { // API 호출 성공
-             log.info("ChatGPT >>> S3 SUCCESS");
-         } else {   // API 호출 실패
-             log.error("ChatGPT >>> S3 FAIL");
+            // S3에 저장할 수 있도록 API 호출
+            DiagnosisDto diagnosisDto = diagnosisService.findDiagnosis();
+            String url = baseUrl + "/s3-save/" + diagnosisDto.getDiagId() + "?email=" + email;
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            HttpEntity httpEntity = new HttpEntity<>(path, headers);
+            RestTemplate restTemplate = new RestTemplate();
+            ResponseEntity<ResultEntity> response = restTemplate.exchange(
+                    url,
+                    HttpMethod.POST,
+                    httpEntity,
+                    ResultEntity.class);
+            if(response.getStatusCode() == HttpStatus.OK) { // API 호출 성공
+                log.info("ChatGPT >>> S3 SUCCESS");
+            } else {   // API 호출 실패
+                log.error("ChatGPT >>> S3 FAIL");
+            }
          }
 
         return summary;
